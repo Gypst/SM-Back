8a5cd37ede3f74fcd4ba7f78c37165feb2f6217c
Gypst <mrdimas27@gmail.com>
2020-11-04 19:38:26 +0700

    Изменил имена миграций в форму YYYYMMDDHHMMSS_Осмысленная_Фраза

--- src/main/java/com/simple/todo/TodoApplication.java
+++ src/main/java/com/simple/todo/TodoApplication.java
@@ -1,9 +1,12 @@
 package com.simple.todo;
 
+import liquibase.pro.packaged.S;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -12,6 +15,10 @@ public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+
+//		System.out.println("Press Enter to exit");
+//		Scanner scanner = new Scanner(System.in);
+//		scanner.next();
 	}
 
 //For testing repository
